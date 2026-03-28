cf258b53d5dde04b79f5a1217a84494f516d4cf9
louisophie <louisophie@gmail.com>
2026-03-28 21:30:38 +0800

    20260328Sat21:29_A5pro

--- injection-site.bash
+++ injection-site.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 export GREP_COLORS='ms=1;37;41'
 cat -n bloodsugar.md | tac | awk '
-  !seen["内"] && /内/      {print $0; seen["内"]=1}
-  !seen["T"]  && /<T|>T/  {print $0; seen["T"]=1}
-  !seen["A"]  && /<A|>A/  {print $0; seen["A"]=1}
-  seen["内"] && seen["T"] && seen["A"] {exit}
-'  | tac | grep -E --color=always "((<|>)(内|T|A))"
+  /####/      && ++c <= 3   { print }
+  /内/        && ++d <= 1   { print; }
+  /<T|>T/     && ++e <= 1   { print; }
+  /<A|>A/     && ++f <= 1   { print; }
+  /<B|>B/     && ++g <= 1   { print; }
+
+  c >= 3 && d >= 2 && e >= 1 && f >= 1 && g >= 1 { exit }
+' | tac | perl -pne 's/^ +(\d+)\t/ $1 /;s/    - //;' | grep -E --color=always "((<|>)(内|T|A|B))|####"
