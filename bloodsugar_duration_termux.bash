6a1ec3324777e511a57f376df57f9df1ac965015
louisophie <louisophie@gmail.com>
2026-08-15 00:30:17 +0800

    20260815Sat00:30_x99

--- bloodsugar_duration_termux.bash
+++ bloodsugar_duration_termux.bash
@@ -3,7 +3,7 @@ termux-wake-lock                    # keep Termux alive
 
 while true; do
     clear                           # optional: clean screen
-    perl ./bloodsugar_duration.pl   # your perl script
+    perl ./bloodsugar_duration_v2.pl
 #    echo "------------------- $(date '+%H:%M:%S') -------------------"
     sleep 60
 done
